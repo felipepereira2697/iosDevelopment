d178258b51a92a928b1a2bfe3bedf24ee3a093c7
felipepereira2697 <felipepereira2697@gmail.com>
2019-12-30 23:08:46 -0300

    Creating table view controller and static table view cells

---
+++ meal-table/meal-table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  meal-table
+//
+//  Created by Felipe Pereira - PERSONAL on 30/12/19.
+//  Copyright © 2019 Felipe Pereira - PERSONAL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
